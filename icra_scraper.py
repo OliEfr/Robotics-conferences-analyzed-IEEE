207fd5c9ca72449afa410205e9a0a584cecc1d23
OliEfr <ollihaus@t-online.de>
2024-05-12 12:23:36 +0200

    .

--- icra_scraper.py
+++ icra_scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import seaborn as sb
+import pandas as pd
 
 
 daily_programs = [
@@ -8,15 +10,12 @@ daily_programs = [
     "https://ras.papercept.net/conferences/conferences/ICRA24/program/ICRA24_ContentListWeb_3.html",
 ]
 
-author_indx = [
-    "https://ras.papercept.net/conferences/conferences/ICRA24/program/ICRA24_AuthorIndexWeb.html"
-]
+author_indx = "https://ras.papercept.net/conferences/conferences/ICRA24/program/ICRA24_AuthorIndexWeb.html"
 
-keyword_indx = [
-    "https://ras.papercept.net/conferences/conferences/ICRA24/program/ICRA24_KeywordIndexWeb.html"
-]
 
-# NOTE I am using the daily program to get the list of contributors. There is alternatively the author_index page that is more accurate.
+keyword_indx = "https://ras.papercept.net/conferences/conferences/ICRA24/program/ICRA24_KeywordIndexWeb.html"
+
+# NOTE I am using the daily program to get the list of contributors. This also includes program chairs and co-chairs. I found that the information under author_index does the same thing.
 def get_university_contributors_list():
     university_list, contributors_list = [], []
     for daily_program in daily_programs:
@@ -41,9 +40,41 @@ def get_university_contributors_list():
 
     return university_list, contributors_list
 
+def get_keywords_dict():
+    keywords_list = []
+
+    response = requests.get(keyword_indx)
+    soup = BeautifulSoup(response.content, "html.parser")
+    # get all rows of table
+    rows = soup.find("table", {"class": "kT"}).find_all("tr")
+    for row in rows:
+        # elements we look for have no attribute
+        if bool(row.attrs):
+            continue
+        else:
+            as_ = row.find_all("a")
+            # this is dirty but it makes it compatible with the data structures for authors and institutions
+            for _ in range(1, len(as_)):
+                keywords_list.append(as_[0].text.strip())
+
+    return keywords_list
+
+def plot(list, title, xlabel, filename):
+    sb.set_theme(rc={'figure.figsize':(15,8.27)})
+    sb.set(font_scale=1.3) 
+    ax = sb.countplot(y=list, order=pd.Series(list).value_counts().iloc[:15].index, color="#485fc7", orient="h")
+    ax.set_xlabel(xlabel)
+    ax.set_title(title, fontsize=20, fontweight='bold', x=.4, y=1.03)
+    ax.get_figure().savefig(filename ,bbox_inches='tight')
 
 university_list, contributors_list = get_university_contributors_list()
-print(contributors_list.count("Levine, Sergey"))
-print(contributors_list.count("Abbeel, Pieter"))
-print(contributors_list.count("Haddadin, Sami"))
+keywords_list = get_keywords_dict()
 
+
+
+
+plot(university_list, "Top 15 Institutions by Contributions", "Number of Contributions", "university_contributors.png")
+
+plot(contributors_list, "Top 15 Authors by Contributions", "Number of Contributions", "authors_contributors.png")
+
+plot(keywords_list, "Top 15 Keywords by Contributions", "Number of Contributions", "keywords_contributors.png")
