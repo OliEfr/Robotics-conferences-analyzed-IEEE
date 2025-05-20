c5b725a086c2af8cb70e86ba58023d1bfc131991
Alexander von Rohr <alex@Alexanders-MacBook-Pro.local>
2025-05-20 14:10:39 +0200

    Add Robotics institute Germany (RIG) partner list and synonyms.

--- institutions_synonyms.py
+++ institutions_synonyms.py
@@ -18,3 +18,178 @@ UT_AUSTIN_SYNONYMS = [
     "UT Austin Robotics",  # Robotics division
     "UT Austin Machine Learning Lab",  # Machine Learning Lab division
 ]
+
+# Synonyms for RIG partners 
+TUM_SYNONYMS = [
+    # Technische Universität München
+    "Technische Universität München",
+    "Technical University of Munich",
+    "Technical University Munich",
+    "Munich Technical University",
+    "Technische Universitaet Muenchen",
+    "Technical University of München",
+    "Technische Universität Muenchen",
+    "TU München",
+    "TU Munich",
+    "TUM",
+    "TUM Munich",
+    "Tech. Univ. Munich",
+]
+
+KIT_SYNONYMS = [
+    # Karlsruher Institut für Technologie
+    "Karlsruher Institut für Technologie",
+    "Karlsruher Institut fuer Technologie",
+    "Karlsruhe Institute of Technology",
+    "Karlsruhe Institute Technology",
+    "Karlsruhe Inst. of Technology",
+    "Karlsruhe Tech Institute",
+    "Institute of Technology Karlsruhe",
+    "Inst. für Technologie Karlsruhe",
+    "KIT",
+]
+
+UNI_BONN_SYNONYMS = [
+    # Rheinische Friedrich-Wilhelms-Universität Bonn
+    "Rheinische Friedrich-Wilhelms-Universität Bonn",
+    "Rheinische Friedrich Wilhelms Universitaet Bonn",
+    "Rhenish Friedrich Wilhelm University of Bonn",
+    "University of Bonn",
+    "Bonn University",
+    "University Bonn",
+    "Universität Bonn",
+    "Uni Bonn",
+]
+
+TU_BERLIN_SYNONYMS = [
+    # Technische Universität Berlin
+    "Technische Universität Berlin",
+    "Technische Universitaet Berlin",
+    "Technical University of Berlin",
+    "Technical University Berlin",
+    "TU Berlin",
+    "TU-Berlin",
+    "Tech. Univ. Berlin",
+]
+
+TU_DARMSTADT_SYNONYMS = [
+    # Technische Universität Darmstadt
+    "Technische Universität Darmstadt",
+    "Technische Universitaet Darmstadt",
+    "Technical University of Darmstadt",
+    "Technical University Darmstadt",
+    "TU Darmstadt",
+    "Tech. Univ. Darmstadt",
+]
+
+UNI_BREMEN_SYNONYMS = [
+    # Universität Bremen
+    "Universität Bremen",
+    "Universitaet Bremen",
+    "University of Bremen",
+    "University Bremen",
+    "Bremen University",
+    "Uni Bremen",
+]
+
+UNI_STUTTGART_SYNONYMS = [
+    # Universität Stuttgart
+    "Universität Stuttgart",
+    "Universitaet Stuttgart",
+    "University of Stuttgart",
+    "University Stuttgart",
+    "Stuttgart University",
+    "Uni Stuttgart",
+]
+
+RWTH_AACHEN_SYNONYMS = [
+    # Rheinisch-Westfälische Technische Hochschule Aachen
+    "Rheinisch-Westfälische Technische Hochschule Aachen",
+    "Rheinisch Westfaelische Technische Hochschule Aachen",
+    "RWTH Aachen University",
+    "RWTH Aachen",
+    "RWTH",
+]
+
+TU_DRESDEN_SYNONYMS = [
+    # Technische Universität Dresden
+    "Technische Universität Dresden",
+    "Technische Universitaet Dresden",
+    "Dresden University of Technology",
+    "Technical University Dresden",
+    "TU Dresden",
+    "Tech. Univ. Dresden",
+]
+
+UTN_SYNONYMS = [
+    # Technische Universität Nürnberg
+    "Technische Universität Nürnberg",
+    "Technische Universitaet Nuernberg",
+    "University of Technology Nuremberg",
+    "Technology University Nuremberg",
+    "TU Nürnberg",
+    "TU Nuremberg",
+    "UTN",
+    "Tech. Univ. Nuremberg",
+]
+
+DLR_SYNONYMS = [
+    # Deutsches Zentrum für Luft- und Raumfahrt
+    "Deutsches Zentrum für Luft- und Raumfahrt",
+    "Deutsches Zentrum fuer Luft- und Raumfahrt",
+    "DLR",
+    "DLR e.V.",
+    "German Aerospace Center",
+    "German Aerospace Center (DLR)",
+]
+
+MPI_IS_SYNONYMS = [
+    # Max-Planck-Institut für Intelligente Systeme
+    "Max-Planck-Institut für Intelligente Systeme",
+    "Max-Planck-Institut fuer Intelligente Systeme",
+    "Max Planck Institute for Intelligent Systems",
+    "Max Planck Institute Intelligent Systems",
+    "MPI for Intelligent Systems",
+    "MPI-IS",
+    "MPI Intelligent Systems",
+]
+
+FRAUNHOFER_IPA_SYNONYMS = [
+    # Fraunhofer-Institut für Produktionstechnik und Automatisierung
+    "Fraunhofer-Institut für Produktionstechnik und Automatisierung",
+    "Fraunhofer-Institut Produktionstechnik und Automatisierung",
+    "Fraunhofer Institute for Manufacturing Engineering and Automation",
+    "Fraunhofer Institute Manufacturing Engineering and Automation",
+    "Fraunhofer Inst. for Manufacturing Engineering and Automation",
+    "Fraunhofer IPA",
+    "FhG IPA",
+]
+
+FRAUNHOFER_IOSB_SYNONYMS = [
+    # Fraunhofer-Institut für Optronik, Systemtechnik und Bildauswertung
+    "Fraunhofer-Institut für Optronik, Systemtechnik und Bildauswertung",
+    "Fraunhofer-Institut fuer Optronik, Systemtechnik und Bildauswertung",
+    "Fraunhofer Institute for Optronics, System Technologies and Image Exploitation",
+    "Fraunhofer Institute for Optronics, Systems Engineering and Image Exploitation",
+    "Fraunhofer IOSB",
+    "FhG IOSB",
+]
+
+FRAUNHOFER_IML_SYNONYMS = [
+    # Fraunhofer-Institut für Materialfluss und Logistik
+    "Fraunhofer-Institut für Materialfluss und Logistik",
+    "Fraunhofer-Institut fuer Materialfluss und Logistik",
+    "Fraunhofer Institute for Material Flow and Logistics",
+    "Fraunhofer Institute Material Flow and Logistics",
+    "Fraunhofer IML",
+    "FhG IML",
+]
+
+DFKI_SYNONYMS = [
+    # Deutsches Forschungszentrum für Künstliche Intelligenz
+    "Deutsches Forschungszentrum für Künstliche Intelligenz",
+    "Deutsches Forschungszentrum fuer Kuenstliche Intelligenz",
+    "German Research Center for Artificial Intelligence",
+    "German Research Centre for Artificial Intelligence",
+    "DFKI",
+]
